aa9107816155535a08cdd551edd53b9fd9cd9ef4
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-21 14:01:11 +0530

    Detailed Syllabus in Data Structure

---
+++ Topics.cpp
@@ -0,0 +1,209 @@
+1) Pointers
+************
+      * Introduction
+      * Pointer Arithmetic
+      * Arrays and Pointers
+      * Characters and Pointers
+      * Pointers and Functions
+      * Double Pointer
+      * MCQs
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+2) Dynamic Allocation
+**********************
+      * Address Typecasting:  i) implicit Typecasting
+                              ii)Explicit Typecasting
+      * Pass By Value & Pass By Reference
+      * Dynamic Memory Allocation:  * Variables
+                                    * Arrays
+      * Dynamic Allocation of 2D-Array
+      * Jagged Array
+      * Macros and Global Variable
+      * Inline and Default Arguments
+      * Constant Variables
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+3) Recursion-1
+***************
+      * Recursion and PMI: * Base case
+                           * Recursive call
+                           * Important calculations
+      * Fibonacci Numbers
+      * Recursion and Arrays
+      problems:
+      ---------
+          * Power
+          * Print Numbers
+          * Number of Digits
+          * Sum of Array
+          * Check Numbers
+          * First index of Number
+          * Last index of Number
+          * All indices of Number
+          * Assignments:    * Multiplication
+                            * Count Zeros
+                            * Geometric Sum
+                            * Check Palindrome
+                            * Sum of Digits
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+4) Assignment: Recursion-1b
+****************************
+      * Replace pi (recursive)
+      * Remove X
+      * String to Integer
+      * Pair Star
+      * Tower of Hanoi
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+5) Recursion-2
+***************
+      * Recursion and Strings
+      * Merge Sort 
+      * Quick Sort
+      * String:  i)  Subsequence
+                 ii) Subset
+                 iii)Permutations
+      problems:
+      ---------
+          * Replace Character recursively
+          * Remove Duplicate recursively
+          * Merge Sort code
+          * Quick Sort code
+          * Return Keypad Code
+          * Print Keypad Combinations code
+          * Assignments:    * Check AB
+                            * Staircase
+                            * Binary Search (recursive)
+                            * Return Subset of an Array
+                            * Print Subsets of Array
+                            * Return Subsets sum to K
+                            * Print Subsets sum to K
+                            * return all code - string
+                            * Print All Codes - string
+                            * Return Permutation - String
+                            * Print Permutations
+      
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+6) Time and Space Complexity Analysis
+**************************************
+      *     
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+7) Test-1
+**********
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+8) OOPs-1
+**********
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+9) OOPs-2
+**********
+
+
+
+
+
+
+*******************************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
