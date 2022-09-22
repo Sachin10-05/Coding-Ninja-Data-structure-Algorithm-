4b3f6450b7eee12bdd2216e25d95b7443b516c63
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-22 08:03:37 +0530

    print numbers from 1 to N.

--- Recursion 1/2. Print Numbers.cpp
+++ Recursion 1/2. Print Numbers.cpp
@@ -19,11 +19,50 @@ Sample Output 2 :
 
 
 
+
 *****************************************************************************************************************************************************************************
 
-//solution:
-************
+//solution-1:
+**************
 
+//Head Recursion:- some processing/ small calculations are done after recursion call.
+-----------------
+
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n)
+{
+   //base case
+   if(n==0)
+      return;
+   
+   //recurrence relation
+   print(n-1);
+   
+   //processing
+   cout << n << " ";
+}
+
+
+int main()
+{
+   int n;
+   cin >> n;
+   print(n);
+   return 0;
+}
+
+
+
+
+
+
+*****************************************************************************************************************************************************************************
+
+//solution-2:
+**************
+
+//Tail Recursion:- some processing/ small calculations are done before recursion call.
 *Note: If we declare a variable as static, it exists till the end of the program once initialized.
 
 #include<bits/stdc++.h>
