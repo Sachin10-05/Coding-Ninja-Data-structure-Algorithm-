63ba4b17a0d5b2c2b9c7e8c5aaa1eb6cbf703135
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-22 15:05:07 +0530

    Remove x from character array.

---
+++ Recursion 1b (assignment)/2. Remove X.cpp
@@ -0,0 +1,99 @@
+/*
+problem
+********
+
+Given a string, compute recursively a new string where all 'x' chars have been removed.
+Sample Input 1 :
+xaxb
+Sample Output 1:
+ab
+
+Sample Input 3 :
+xx
+Sample Output 3:
+
+*/
+
+
+
+
+
+//*************************************************************************************************************************************************************************
+
+
+//solution-1
+*************
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(char arr[], char ans[])
+{
+   static int i=0;
+    
+   //base case
+   if(arr[0]=='\0')
+      return i;
+   if(arr[0] != 'x')
+   {
+      ans[i] =  arr[0];
+      i++;
+   }
+   return helper(arr+1, ans);
+}
+
+
+void removeX(char arr[])
+{
+   char ans[100];
+   int size = helper(arr,ans);
+   cout << size << endl;
+   for(int i=0;i<size;i++)
+   {
+      arr[i] = ans[i];
+   }
+   arr[size] = '\0';
+}
+
+
+int main() 
+{
+   char input[100];
+   cin.getline(input, 100);
+   removeX(input);
+   cout << input << endl;
+}
+
+
+
+
+
+
+
+***************************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
