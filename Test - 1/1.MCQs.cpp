8951ec4b652db29a0ad1e761cb390318d9e7362c
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-26 07:55:29 +0530

    MCQs

---
+++ Test - 1/1.MCQs.cpp
@@ -0,0 +1,28 @@
+1) The recurrence relation capturing the optimal execution time of the Towers of Hanoi problem with n discs is :
+   a) T(n) = 2T(n − 2) + 2
+   b) T(n) = 2T(n − 1) + n
+   c) T(n) = 2T(n/2) + 1
+   d) T(n) = 2T(n − 1) + 1 ✔
+   
+   
+**************************************************************************************************************************************************************************
+
+2) Which of the following operations is not O(1) for an array of sorted data. You may assume that array elements are distinct.
+   a) Find the ith largest element
+   b) Delete an element ✔
+   c) Find the ith smallest element
+   d) All of the above
+   
+   
+**************************************************************************************************************************************************************************
+
+3) Which one of the following correctly determines the solution of the recurrence relation with T(1) = 1?
+   T(n) = 2T(n/2) + Logn
+   
+   a) O(N) ✔
+   b) O(NlogN)
+   c) O(N^2)
+   d) O(log N)
+
+     
+*************************************************************************************************************************************************************************
