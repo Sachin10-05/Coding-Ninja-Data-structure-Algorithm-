17e03ea6674634a3df5752f75afef7a5a43f6bfb
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-28 10:49:36 +0530

    MCQs.

---
+++ Linked List 2/MCQs.cpp
@@ -0,0 +1,76 @@
+1) Circular doubly LL:
+   Given an unsorted circular doubly linked list, suppose you have reference (or pointer) to its head node only, 
+   which of the following operation can be implemented in O(1) time?
+   i) Insertion at the front of the linked list
+  ii) Insertion at the end of the linked list
+ iii) Deletion of the last node of the linked list
+  iv) Deletion of the front node of the linked list
+  
+  a) I and II
+  b) I,II and III
+  c) I,II,III and IV ✔
+  d) None
+  
+  
+******************************************************************************************************************************************************************************
+
+2) In doubly linked lists, traversal can be done in ?
+   a) Only forward direction
+   b) Only reverse direction
+   c) Both directions ✔
+   d) None of the above
+   
+   
+******************************************************************************************************************************************************************************
+
+3) Double LL:
+   Given an unsorted doubly Linked List, suppose you have references (or pointer) to its head and tail nodes, which of the following operation can be implemented in O(1) time ?
+   i) Insertion at the front of the linked list
+  ii) Insertion at the end of the linked list
+ iii) Deletion of the last node of the linked list
+  iv) Deletion of the front node of the linked list
+  
+  a) I and II
+  b) I and III
+  c) I,II and III
+  d) I,II, III and IV ✔
+  
+  
+*****************************************************************************************************************************************************************************
+
+4) Circular LL:
+   Given an unsorted circular linked list, suppose you have reference (or pointer) to its head node only, which of the following operation can be implemented in O(1) time?
+   i) Insertion at the front of the linked list
+  ii) Insertion at the end of the linked list
+ iii) Deletion of the last node of the linked list
+  iv) Deletion of the front node of the linked list
+  
+  a) I and II
+  b) I and III
+  c) I,II and III
+  d) None ✔
+
+
+*****************************************************************************************************************************************************************************
+
+
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
