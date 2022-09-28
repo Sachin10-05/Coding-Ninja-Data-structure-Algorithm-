5e47a10bd8709fc3717149bc5123582e4fcaae50
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-28 23:44:44 +0530

    Assignment.

--- Linked List 2/Even after Odd LL.cpp
+++ Linked List 2/Even after Odd LL.cpp
@@ -82,55 +82,55 @@ Node *evenAfterOdd(Node *head)
 solution-2:
 ************
 
-
+/*
 #include <bits/stdc++.h>
 using namespace std;
 class Node
 {
    public:
    int data;
-	 Node *next;
-	 Node(int data)
-	 {
-	    this->data = data;
-		  this->next = NULL;
-	 }
+   Node *next;
+   Node(int data)
+   {
+      this->data = data;
+      this->next = NULL;
+   }
 };
 
 
 Node *takeinput()
 {
    int data;
-	 cin >> data;
-	 Node *head = NULL, *tail = NULL;
-	 while (data != -1)
-	 {
-	    Node *newnode = new Node(data);
-		  if (head == NULL)
-		  {
-			   head = newnode;
-			   tail = newnode;
-		  }
-		  else
-		  {
-			   tail->next = newnode;
-			   tail = newnode;
-		  }
-		  cin >> data;
-	 }
-	 return head;
+   cin >> data;
+   Node *head = NULL, *tail = NULL;
+   while (data != -1)
+   {
+      Node *newnode = new Node(data);
+      if (head == NULL)
+      {
+         head = newnode;
+	 tail = newnode;
+      }
+      else
+      {
+         tail->next = newnode;
+	 tail = newnode;
+      }
+      cin >> data;
+   }
+   return head;
 }
 
 
 void print(Node *head)
 {
    Node *temp = head;
-	 while (temp != NULL)
-	 {
-	    cout << temp->data << " ";
-		  temp = temp->next;
-	 }
-	 cout << endl;
+   while (temp != NULL)
+   {
+      cout << temp->data << " "; 
+      temp = temp->next;
+   }
+   cout << endl;
 }
 
 
@@ -204,16 +204,17 @@ Node *evenAfterOdd(Node *head)
 int main()
 {
    int t;
-	 cin >> t;
-	 while (t--)
-	 {
-	    Node *head = takeinput();
-		  head = evenAfterOdd(head);
-		  print(head);
-	 }
-	 return 0;
+   cin >> t;
+   while (t--)
+   {
+      Node *head = takeinput();
+      head = evenAfterOdd(head);
+      print(head);
+   }
+   return 0;
 }
-
+*/
+	
   
 
 
@@ -221,6 +222,10 @@ int main()
 
 
 **************************************************************************************************************************************************************************
+***************************************************************************************************************************************************************************
+***************************************************************************************************************************************************************************
+***************************************************************************************************************************************************************************
+***************************************************************************************************************************************************************************
 
 
 
