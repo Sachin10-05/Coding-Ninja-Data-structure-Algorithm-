d775a0ec4ddda753016aa3ed2adf39b69ccc6b9f
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-29 23:30:23 +0530

    BUBBLE SORT in LL.

--- Linked List 2/Bubble Sort (iterative).cpp
+++ Linked List 2/Bubble Sort (iterative).cpp
@@ -106,60 +106,66 @@ Node *bubbleSort(Node *head)
 //main-code:
 -------------
 
+/*
 #include <iostream>
+using namespace std;
+
 class Node
 {
-  public:
-	int data;
-	Node *next;
-	Node(int data)
-	{
-	   this->data = data;
-		 this->next = NULL;
-	}
+   public:
+   int data;
+   Node *next;
+   Node(int data)
+   {
+      this->data = data;
+      this->next = NULL;
+   }
 };
 
-using namespace std;
-#include "solution.h"
+
 Node *takeinput()
 {
-	int data;
-	cin >> data;
-	Node *head = NULL, *tail = NULL;
-	while (data != -1)
-	{
-		Node *newnode = new Node(data);
-		if (head == NULL)
-		{
-			head = newnode;
-			tail = newnode;
-		}
-		else
-		{
-			tail->next = newnode;
-			tail = newnode;
-		}
-		cin >> data;
-	}
-	return head;
-}
-void print(Node *head)
-{
-	Node *temp = head;
-	while (temp != NULL)
-	{
-		cout << temp->data << " ";
-		temp = temp->next;
-	}
-	cout << endl;
-}
-int main()
-{
-	Node *head = takeinput();
-	head = bubbleSort(head);
-	print(head);
+   int data;
+   cin >> data;
+   Node *head = NULL, *tail = NULL;
+   while (data != -1)
+   {
+      Node *newnode = new Node(data);
+      if (head == NULL)
+      {
+         head = newnode;
+	 tail = newnode;
+      }
+      else
+      {
+         tail->next = newnode;
+	 tail = newnode;
+      }
+      cin >> data;
+   }
+   return head;
 }
 
+
+void print(Node *head)
+{
+   Node *temp = head;
+   while (temp != NULL)
+   {
+      cout << temp->data << " ";
+      temp = temp->next;
+   }
+   cout << endl;
+}
+
+
+int main()
+{
+   Node *head = takeinput();
+   head = bubbleSort(head);
+   print(head);
+}
+*/
 
 
 
