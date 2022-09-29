96daab3b6aa560cb5f4bd83b1c8b2b54c0998f67
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-29 22:36:16 +0530

    Assignment.

--- Linked List 2/Delete every N nodes.cpp
+++ Linked List 2/Delete every N nodes.cpp
@@ -89,73 +89,77 @@ Node *skipMdeleteN(Node *head, int m, int n)
 
 //main-code:
 
-
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 class Node
 {
    public:
-	 int data;
-	 Node *next;
-	 Node(int data)
-	 {
-	    this->data = data;
-		  this->next = NULL;
-	 }
+   int data;
+   Node *next;
+   Node(int data)
+   {
+      this->data = data;
+      this->next = NULL;
+   }
 };
 
 
 
 Node *takeinput()
 {
-	int data;
-	cin >> data;
-	Node *head = NULL, *tail = NULL;
-	while (data != -1)
-	{
-		Node *newnode = new Node(data);
-		if (head == NULL)
-		{
-			head = newnode;
-			tail = newnode;
-		}
-		else
-		{
-			tail->next = newnode;
-			tail = newnode;
-		}
-		cin >> data;
-	}
-	return head;
+   int data;
+   cin >> data;
+   Node *head = NULL, *tail = NULL;
+   while (data != -1)
+   {
+      Node *newnode = new Node(data);
+      if (head == NULL)
+      {
+         head = newnode;
+	 tail = newnode;
+      }
+      else
+      {
+         tail->next = newnode;
+	 tail = newnode;
+      }
+      cin >> data;
+   }
+   return head;
 }
+
+
 
 void print(Node *head)
 {
-	Node *temp = head;
-	while (temp != NULL)
-	{
-		cout << temp->data << " ";
-		temp = temp->next;
-	}
-	cout << endl;
+   Node *temp = head;
+   while (temp != NULL)
+   {
+      cout << temp->data << " ";
+      temp = temp->next;
+   }
+   cout << endl;
 }
+
+
 
 int main()
 {
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		Node *head = takeinput();
-		int m, n;
-		cin >> m >> n;
-		head = skipMdeleteN(head, m, n);
-		print(head);
-	}
-	return 0;
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      Node *head = takeinput();
+      int m, n;
+      cin >> m >> n;
+      head = skipMdeleteN(head, m, n);
+      print(head);
+   }
+   return 0;
 }
-
+*/
 
 
 
