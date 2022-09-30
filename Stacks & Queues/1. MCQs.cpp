e181e344b89cd1dde5dce9b73d46d21791356ea0
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-30 12:45:18 +0530

    MCQs.

---
+++ Stacks & Queues/1. MCQs.cpp
@@ -0,0 +1,99 @@
+//STACKS
+*********
+
+1) A stack is a ?
+   a) FIFO (First In First Out) list
+   b) LIFO (Last In First Out) list. ✔
+   c) Ordered array
+   d) Linear tree
+   
+   
+*******************************************************************************************************************************************************************************
+2) Which data structure is used for implementing recursion?
+   a) Queue
+   b) Stack ✔
+   c) Array
+   d) List
+   
+   
+*******************************************************************************************************************************************************************************
+3) Which of the following statement(s) about stack data structure is/are NOT correct?
+   a) Stack data structure can be implemented using linked list
+   b) New node can only be added at the top of the stack
+   c) Stack is the FIFO data structure ✔
+   d) New node can be added at both ends ✔
+   
+   
+*******************************************************************************************************************************************************************************
+4) Which of the following operation take worst case i.e. linear time (O(n)) in the array implementation of stack?
+   a) Push
+   b) Pop
+   c) IsEmpty
+   d) None ✔
+   
+   
+*******************************************************************************************************************************************************************************
+5) If the elements “p”, “q”, “r” and “s” are placed in a stack and are deleted one at a time, in what order will they be removed?
+   a) srqp ✔
+   b) pqrs
+   c) srpq
+   d) qrsp
+   
+   
+*******************************************************************************************************************************************************************************
+*******************************************************************************************************************************************************************************
+
+
+//QUEUES
+*********
+
+1) A queue is a ?
+   a) FIFO (First In First Out) list ✔
+   b) LIFO (Last In First Out) list
+   c) Ordered array
+   d) Linear tree
+   
+   
+*******************************************************************************************************************************************************************************
+2) If the elements “p”, “q”, “r” and “s” are placed in a queue and are deleted one at a time, in what order will they be removed?
+   a) srqp
+   b) pqrs ✔
+   c) srpq
+   d) qrsp
+   
+   
+*******************************************************************************************************************************************************************************
+3) In optimized linked list implementation of a queue, where does a new element be inserted?
+   a) At the head of linked list
+   b) At the tail of the linked list ✔
+   c) At the centre position in the linked list
+   d) None
+   
+   
+*******************************************************************************************************************************************************************************
+4) In linked list implementation of queue, if only front pointer is maintained, which of the following operation take worst case time (i.e. O(n) )?
+   a) Insertion from rear ✔
+   b) Deletion from front
+   c) Both
+   d) None
+   
+   
+*******************************************************************************************************************************************************************************
+5) In linked list implementation of a queue, front and rear pointers are tracked. Which of these pointers will change during an insertion into EMPTY queue?
+   a) Only front pointer
+   b) Only rear pointer
+   c) Both front and rear pointer ✔
+   d) None
+   
+   
+*******************************************************************************************************************************************************************************
+*******************************************************************************************************************************************************************************
+ 
+   
+   
+   
+   
+   
+   
+   
+   
