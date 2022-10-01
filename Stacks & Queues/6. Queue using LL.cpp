474235585978ac7294e75280f2e5d4623c6a8aac
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-10-01 15:36:39 +0530

    Code.

--- Stacks & Queues/6. Queue using LL.cpp
+++ Stacks & Queues/6. Queue using LL.cpp
@@ -8,9 +8,9 @@ using namespace std;
 class Node 
 {
    public :
-	 int data;
+   int data;
    Node *next;
-	 Node(int data) 
+   Node(int data) 
    {
       this->data = data;
       next = NULL;
@@ -22,64 +22,65 @@ class Node
 class Queue
 {
    private:
-	 Node *head;
-	 Node *tail;
-	 int size;
+   Node *head;
+   Node *tail;
+   int size;
 		
-	 public:
-	 Queue()
-	 {
-	    head = NULL;
-			tail = NULL;
-			size = 0;
-	 }
+   public:
+   //Default constructor
+   Queue()
+   {
+      head = NULL;
+      tail = NULL;
+      size = 0;
+   }
 		
-	 int getSize()
-	 {
-	    return size;
-	 }
+   int getSize()
+   {
+      return size;
+   }
 		
-	 bool isEmpty()
-	 {
-	    if(size == 0)
-			   return true;
-			return false;
-	 }
+   bool isEmpty()
+   {
+      if(size == 0)
+         return true;
+      return false;
+   }
 		
-	 void enqueue(int element)
-	 {
-	    Node *n = new Node(element);
-			if(head == NULL)
-			{
-			   head = n;
-				 tail = n;
-			}
-			else
-			{
-			   tail->next = n;
-				 tail = n;
-			}
-			size++;
-	 }
+   void enqueue(int element)
+   {
+      Node *n = new Node(element);
+      if(head == NULL)
+      {
+         head = n;
+	 tail = n;
+      }
+      else
+      {
+         tail->next = n;
+	 tail = n;
+      }
+      size++;
+   }
 		
-	 int dequeue()
-	 {
-	    if(head == NULL)
-			   return -1;
-			int ans = head->data;
-			Node *temp = head;
-			head = head->next;	
-			size--;
-			delete temp;
-			return ans;
-	 }
+   int dequeue()
+   {
+      if(head == NULL)
+         return -1;
+      int ans = head->data;
+      Node *temp = head;
+      head = head->next;	
+      size--;
+      delete temp;
+      return ans;
+   }
 		
-	 int front()
-	 {
-	    if(head == NULL)
-			   return -1;
-			return head->data;
-	 }
+   int front()
+   {
+      if(head == NULL)
+         return -1;
+      return head->data;
+   }
 };
 
 
