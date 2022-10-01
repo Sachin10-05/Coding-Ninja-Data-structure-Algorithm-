c2d989edf25043f86cce3726e6ac5ed3fc3e7d16
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-10-01 15:32:07 +0530

    Code.

---
+++ Stacks & Queues/6. Queue using LL.cpp
@@ -0,0 +1,102 @@
+//solution:
+************
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node 
+{
+   public :
+	 int data;
+   Node *next;
+	 Node(int data) 
+   {
+      this->data = data;
+      next = NULL;
+   }
+};
+
+
+
+class Queue
+{
+   private:
+	 Node *head;
+	 Node *tail;
+	 int size;
+		
+	 public:
+	 Queue()
+	 {
+	    head = NULL;
+			tail = NULL;
+			size = 0;
+	 }
+		
+	 int getSize()
+	 {
+	    return size;
+	 }
+		
+	 bool isEmpty()
+	 {
+	    if(size == 0)
+			   return true;
+			return false;
+	 }
+		
+	 void enqueue(int element)
+	 {
+	    Node *n = new Node(element);
+			if(head == NULL)
+			{
+			   head = n;
+				 tail = n;
+			}
+			else
+			{
+			   tail->next = n;
+				 tail = n;
+			}
+			size++;
+	 }
+		
+	 int dequeue()
+	 {
+	    if(head == NULL)
+			   return -1;
+			int ans = head->data;
+			Node *temp = head;
+			head = head->next;	
+			size--;
+			delete temp;
+			return ans;
+	 }
+		
+	 int front()
+	 {
+	    if(head == NULL)
+			   return -1;
+			return head->data;
+	 }
+};
+
+
+
+
+
+
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
