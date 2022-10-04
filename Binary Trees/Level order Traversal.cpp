4183db5d84e4855f4c13a7cfe3168ec8a9029417
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-10-04 09:30:48 +0530

    Assignment.

---
+++ Binary Trees/Level order Traversal.cpp
@@ -0,0 +1,133 @@
+/*
+problem:
+********
+For a given a Binary Tree of type integer, print it in a level order fashion where each level will be printed on a new line. 
+Elements on every level will be printed in a linear fashion and a single space will separate them.
+Ex-      10
+        /  \
+       20  30
+      /  \   \
+     40 50    60
+Ans:- 10
+      20 30 
+      40 50 60
+      
+Sample Input 1:
+10 20 30 40 50 -1 60 -1 -1 -1 -1 -1 -1 
+Sample Output 1:
+10 
+20 30 
+40 50 60 
+
+Sample Input 2:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+Sample Output 2:
+8 
+3 10 
+1 6 14 
+4 7 13 
+*/
+
+
+
+
+
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+
+//solution:
+************
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode
+{
+};
+
+
+
+BinaryTreeNode<int>* takeInput()
+{
+}
+
+
+
+void printLevelWise(BinaryTreeNode<int> *root) 
+{
+   // Write your code here
+   //base case
+   if(root == NULL)
+      return;
+   queue<BinaryTreeNode<int> *> pendingNodes;
+   pendingNodes.push(root);
+   while (pendingNodes.size() != 0)
+   {
+      int n = pendingNodes.size();
+      while (n != 0)
+      {
+         BinaryTreeNode<int> *front = pendingNodes.front();
+         pendingNodes.pop();
+         cout << front->data << " ";
+         if (front->left != NULL)
+         {
+            pendingNodes.push(front->left);
+         }
+         if (front->right != NULL)
+         {
+            pendingNodes.push(front->right);
+         }
+         n--;
+      }
+      cout << "\n";
+   }
+}
+
+
+
+
+int main() 
+{
+   BinaryTreeNode<int>* root = takeInput();
+   printLevelWise(root);
+}
+
+
+
+
+
+
+
+
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
