c170ef3815751e0c57acdc1058e641f170e92589
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-10-04 18:32:34 +0530

    Code.

---
+++ Binary Trees/9. Min & Max of Binary Tree.cpp
@@ -0,0 +1,122 @@
+/*
+problem:
+********
+For a given a Binary Tree of type integer, find and return the minimum and the maximum data values.
+Return the output as an object of Pair class, which is already created.
+
+Note: All the node data will be unique and hence there will always exist a minimum and maximum node data.
+
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+Sample Output 1:
+1 14
+
+Sample Input 2:
+10 20 60 -1 -1 3 50 -1 -1 -1 -1 
+Sample Output 2:
+3 60
+*/
+
+
+
+
+
+
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+
+//solution:
+***********
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+template <typename T>
+class BinaryTreeNode 
+{
+};
+
+
+
+BinaryTreeNode<int>* takeInput()
+{
+}
+
+
+
+
+#include <climits>
+void Min(BinaryTreeNode<int> *root, int *min)
+{
+   if (root == NULL)
+      return;
+   if ((*min) > root->data)
+      *min = root->data;
+   Min(root->left, min);
+   Min(root->right, min);
+}
+
+void Max(BinaryTreeNode<int> *root, int *max)
+{
+   if (root == NULL)
+      return;
+   if ((*max) < root->data)
+      *max = root->data;
+   Max(root->left, max);
+   Max(root->right, max);
+}
+
+pair<int, int> getMinAndMax(BinaryTreeNode<int> *root)
+{
+   // Write your code here
+   pair<int, int> p = {0, 0};
+   if (root == NULL)
+      return p;
+   int min = INT_MAX, max = INT_MIN;
+   Min(root, &min);
+   Max(root, &max);
+   p = {min, max};
+   return p;
+}
+
+
+
+
+
+int main() 
+{
+   BinaryTreeNode<int>* root = takeInput();
+   pair<int, int> ans = getMinAndMax(root);
+   cout << ans.first << " " << ans.second;
+}
+
+
+
+
+
+
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+****************************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
