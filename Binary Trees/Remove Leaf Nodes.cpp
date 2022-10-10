1d68f16f01eaf814a2f99c07e7870ec66cfd9df9
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-10-10 19:15:50 +0530

    Assignment.

--- Binary Trees/Remove Leaf Nodes.cpp
+++ Binary Trees/Remove Leaf Nodes.cpp
@@ -64,15 +64,8 @@ BinaryTreeNode<int>* removeLeafNodes(BinaryTreeNode<int> *root)
    if(root->left==NULL && root->right==NULL)
       return NULL;
     
-   if(root->left != NULL)
-   {
-      root->left = removeLeafNodes(root->left);
-   }
-    
-   if(root->right != NULL)
-   {
-      root->right = removeLeafNodes(root->right);
-   }
+   root->left = removeLeafNodes(root->left);
+   root->right = removeLeafNodes(root->right);
    return root;
 }
 
