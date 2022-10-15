63294d0f4aa31b68333396162e91d97aeaf89ccf
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-10-15 08:46:20 +0530

    Code - insertion, search, deletion, etc.

--- BST/7. BST Class.cpp
+++ BST/7. BST Class.cpp
@@ -32,7 +32,164 @@ Implement the BST class which includes following functions -
 //solution:
 ***********
 
+class BST
+{
+   //complete this class
+   private:
+      BinaryTreeNode<int>* root;   //root node
+    
+   public:
+      //constructor
+    	BST()
+    	{
+        	root = NULL;
+    	}
+    
+    	//destructor
+    	~BST()
+    	{
+        	delete root;
+    	}
+    
+    
+    
+   //operation-1: SEARCH
+   private:
+      bool search(BinaryTreeNode<int>* node, int data) 
+      { 
+         //base case
+			if (node == NULL) 
+            return false; 
+            
+			if (node->data == data) 
+            return true;
+			else if (data < node->data) 
+            return search(node->left, data);
+			return search(node->right, data);
+		}
 
+    
+   //operation-2: PRINT
+   private:
+      void printTree(BinaryTreeNode<int>* root)
+      {
+         //base case
+         if(root == NULL)
+            return;
+         cout << root->data << ":";
+         if(root->left != NULL)
+         {
+            cout << "L:" << root->left->data << ",";
+         }
+         if(root->right != NULL)
+         {
+            cout << "R:" << root->right->data;
+         }
+         cout << endl;
+         printTree(root->left);
+         printTree(root->right);
+     }
+    
+    
+   //operation-3: INSERTION
+   private:
+      BinaryTreeNode<int>* insert(BinaryTreeNode<int>* root, int data)
+      {
+         if (root == NULL)
+        	{
+            BinaryTreeNode<int> *temp = new BinaryTreeNode<int>(data);
+            return temp;
+        	}
+         if (data <= root->data)
+            root->left = insert(root->left, data);
+        	else
+            root->right = insert(root->right, data);
+			return root;
+     }
+    
+    
+   //operation-4: DELETION
+   private:
+      BinaryTreeNode<int>* minVal(BinaryTreeNode<int>* root)
+      {
+         if(root->left==NULL)
+            return root;
+      }
+    
+      BinaryTreeNode<int>* remove(BinaryTreeNode<int>* root, int val)
+      {
+         //base case
+         if(root == NULL)
+            return root;
+            
+         if(root->data == val)
+         {
+            //case-1: 0 children 
+            if(root->left==NULL && root->right==NULL)
+            {
+               delete root;
+               return NULL;
+            }
+                
+            //case-2: 1 children (left side)
+            if(root->left!=NULL && root->right==NULL)
+            {
+               BinaryTreeNode<int>* temp = root->left;
+               delete root;
+               return temp;
+            }
+                
+            //case-3: 1 children (right side)
+            if(root->left==NULL && root->right!=NULL)
+            {
+               BinaryTreeNode<int>* temp = root->right;
+               delete root;
+               return temp;
+            }
+                
+            //case-4: 2 children
+            if(root->left!=NULL && root->right!=NULL)
+            {
+               int min = minVal(root->right)->data;
+               root->data = min;
+               root->right = remove(root->right, min);
+               return root;
+            }
+         }
+         else if(val > root->data)
+         {
+            root->right = remove(root->right, val);
+         }
+         else
+         {
+            root->left = remove(root->left, val);
+         }
+     }
+    
+    
+    
+   public:
+   //function call
+   void remove(int data)
+   {
+      root = remove(root, data);
+   }
+    
+   void print()
+   {
+      printTree(root);
+   }
+    
+   bool search(int val)
+   {
+      return search(root, val);
+   }
+    
+   void insert(int data)
+   {
+      root = insert(root, data);
+   }
+};
 
 
 
